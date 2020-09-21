091c1aad5e3ffa6f1171187c3a84d184e986f937
Wei Ding <DeanWade@users.noreply.github.com>
2020-09-21 13:22:07 +0800

    init commit

---
+++ network_connection_host.py
@@ -0,0 +1,23 @@
+import psutil
+
+from ruxit.api.base_plugin import BasePlugin
+
+class NetworkExtension(BasePlugin):
+    def query(self, **kwargs):
+        try:
+            value = None
+            value = len(psutil.net_connections())
+            if value is not None:
+                self.results_builder.add_absolute_result(
+                    PluginMeasurement(key="network_connection", value=value, entity_selector=FromPluginSelector())
+                )                
+        except Exception as e:
+            error = f"Generic command - error running command: {e}"
+            self.results_builder.report_error_event(error, "Network Extension Error")
+
+
+if __name__ == "__main__":
+    value = None
+    value = len(psutil.net_connections())
+    if value is not None:
+        print(value)
